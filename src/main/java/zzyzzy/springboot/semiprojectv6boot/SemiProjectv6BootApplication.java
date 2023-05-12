f7a1cc31fd14dda33bdd615b67d6052bdac05433
hoteaklee <cc18ht@naver.com>
2023-05-12 10:58:43 +0900

    프로젝트 초기화

---
+++ src/main/java/zzyzzy/springboot/semiprojectv6boot/SemiProjectv6BootApplication.java
@@ -0,0 +1,13 @@
+package zzyzzy.springboot.semiprojectv6boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiProjectv6BootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemiProjectv6BootApplication.class, args);
+    }
+
+}
