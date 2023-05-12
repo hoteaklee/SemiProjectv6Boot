c987b3cf7deb277ff9cc09a5a81c8e7247b01271
hoteaklee <cc18ht@naver.com>
2023-05-12 12:12:03 +0900

    프로젝트 패키지명 변경

--- src/main/java/zzyzzy/springboot/semiprojectv6/controller/JoinController.java
+++ src/main/java/zzyzzy/springboot/semiprojectv6/controller/JoinController.java
@@ -1,4 +1,4 @@
-package zzyzzy.springboot.semiprojectv6boot.controller;
+package zzyzzy.springboot.semiprojectv6.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-import zzyzzy.springboot.semiprojectv5.model.Member;
-import zzyzzy.springboot.semiprojectv5.service.JoinService;
+import zzyzzy.springboot.semiprojectv6.model.Member;
+import zzyzzy.springboot.semiprojectv6.service.JoinService;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
